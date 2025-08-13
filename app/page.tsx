54a166dcd31c57dc7fdb9adc6a4b73e768661359
Ngan <ngannguyen141123@gmail.com>
2025-08-13 16:28:12 +1000

    add footer component with style

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 // app/page.tsx
 import HamburgerMenu from "./Components/HamburgerMenu";
-
+import Footer from "./Components/Footer"
 export default function Home() {
   return (
     <div>
       <HamburgerMenu />
       <h1>Welcome to My Next.js App</h1>
+      <Footer/>
     </div>
   );
 }
