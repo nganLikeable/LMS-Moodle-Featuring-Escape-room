e427dd94bc500d0cab3410e46e9ddca939ccf01f
Ngan <ngannguyen141123@gmail.com>
2025-08-13 15:53:53 +1000

    add hamburger menu component

---
+++ app/Components/HamburgerMenu.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useState } from "react";
+import styles from "./HamburgerMenu.module.css";
+
+const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <div className={isOpen ? styles.barOpen : styles.bar}></div>
+        <div className={isOpen ? styles.barOpen : styles.bar}></div>
+        <div className={isOpen ? styles.barOpen : styles.bar}></div>
+      </div>
+      <nav className={isOpen ? styles.menuOpen : styles.menu}>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
---
+++ app/Components/Header.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const Header = () => {
+  return (
+    <div>
+      <div id="student-info">
+        <p>Title</p>
+        <p>21519232</p>
+      </div>
+      <nav></nav>
+    </div>
+  );
+};
