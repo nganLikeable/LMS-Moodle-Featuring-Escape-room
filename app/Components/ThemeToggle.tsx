bcc7c0e3897b46ef3ccd99a7cdbe7c97fd7b7a8d
Ngan <ngannguyen141123@gmail.com>
2025-08-27 20:38:18 +1000

    fixed hydration error by adding mounted state

--- app/Components/ThemeToggle.tsx
+++ app/Components/ThemeToggle.tsx
@@ -1,9 +1,19 @@
 "use client";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { RiMoonLine, RiSunLine } from "react-icons/ri";
 import styles from "./ThemeToggle.module.css";
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
+  // prevent hydration error
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // render nothing on server
+
   return (
     <div className={styles.container}>
       <button
