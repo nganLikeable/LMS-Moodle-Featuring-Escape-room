a7ead2ceefeb1d7dcbdd1f85a116982c83b8cc27
Ngan <ngannguyen141123@gmail.com>
2025-08-27 18:12:36 +1000

    added themes to output, refactored syntaxhighlighterProps for reusing

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <ThemeProvider
           attribute="data-theme"
