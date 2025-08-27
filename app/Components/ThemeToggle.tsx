74cff182f964d02919baa6fd2e86685b643cf5b8
Ngan <ngannguyen141123@gmail.com>
2025-08-27 10:10:23 +1000

    added toggle using tailwind

---
+++ app/Components/ThemeToggle.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useTheme } from "next-themes";
+import { RiMoonLine, RiSunLine } from "react-icons/ri";
+
+const DarkModeToggle = () => {
+  const { theme, setTheme } = useTheme();
+  return (
+    <button
+      onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
+    >
+      {theme === "light" ? <RiMoonLine /> : <RiSunLine />}
+    </button>
+  );
+};
+
+export default DarkModeToggle;
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ import NavBar from "./Components/NavBar";
 import Output from "./Components/Output";
 import TabContainer from "./Components/TabContainer";
 import Tabs from "./Components/Tabs";
+import DarkModeToggle from "./Components/ThemeToggle";
 export default function Home() {
   return (
     <div>
       <Header />
       <hr></hr>
+      <DarkModeToggle />
       <NavBar />
       <Tabs />
       <TabContainer />
