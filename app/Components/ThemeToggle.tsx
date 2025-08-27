40cc9a122c4b2227e7b6a306baae26b2b37dadd4
Ngan <ngannguyen141123@gmail.com>
2025-08-27 12:37:40 +1000

    added dark mode for flex boxes and input fields

--- app/Components/ThemeToggle.tsx
+++ app/Components/ThemeToggle.tsx
@@ -1,14 +1,16 @@
 "use client";
 import { useTheme } from "next-themes";
 import { RiMoonLine, RiSunLine } from "react-icons/ri";
-
+import styles from "./ThemeToggle.module.css";
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
   return (
-    <button
-      onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
-    >
-      {theme === "light" ? <RiMoonLine /> : <RiSunLine />}
-    </button>
+    <div className={styles.container}>
+      <button
+        onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
+      >
+        {theme === "light" ? <RiMoonLine /> : <RiSunLine />}
+      </button>
+    </div>
   );
 }
