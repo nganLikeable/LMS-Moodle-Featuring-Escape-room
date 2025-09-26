50a1b51b5f59608242bd52dfed529500d5da23d5
Ngan <ngannguyen141123@gmail.com>
2025-09-26 10:33:52 +1000

    added timer

--- app/Components/Stage1.tsx
+++ app/Components/Stage1.tsx
@@ -1,8 +1,12 @@
+import Timer from "@/app/Components/Timer";
+
 export default function Stage1() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h2>Stage 1: Format the Code</h2>
       <p>Your puzzle goes here...</p>
+
+      <Timer onTimeUp={() => alert("Time's up!")} />
       <button>Next Stage</button>
     </div>
   );
--- app/Components/Timer.tsx
+++ app/Components/Timer.tsx
@@ -1,15 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 type TimerProps = {
-  initialSeconds: number;
   onTimeUp: () => void;
 };
-export default function Timer({ initialSeconds, onTimeUp }: TimerProps) {
-  const [timeLeft, setTimeLeft] = useState(initialSeconds);
+
+export default function Timer({ onTimeUp }: TimerProps) {
+  const [customMinutes, setCustomMinutes] = useState(5);
+  const [timeLeft, setTimeLeft] = useState(customMinutes * 60);
   const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    if (!isRunning) return;
+
+    if (timeLeft <= 0) {
+      setIsRunning(false);
+      onTimeUp();
+      return;
+    }
+
+    // set time left every 1s (1000ms)
+    const interval = setInterval(() => {
+      setTimeLeft((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [isRunning, timeLeft, onTimeUp]);
+
+  // keep time left in sync in sec
+  useEffect(() => {
+    setTimeLeft(customMinutes * 60);
+  }, [customMinutes]);
+
+  // format time
+  const minutes = Math.floor(timeLeft / 60)
+    .toString()
+    .padStart(2, "0");
+  const seconds = (timeLeft % 60).toString().padStart(2, "0");
+
   return (
     <div>
-      <button>Start</button>
-      <button></button>
+      <h3>
+        Timer: {minutes}:{seconds}
+      </h3>
+      <input
+        type="number"
+        value={customMinutes}
+        onChange={(e) => setCustomMinutes(Number(e.target.value))}
+        disabled={isRunning}
+      ></input>
+      <span>minutes</span>
+      <button onClick={() => setIsRunning(true)}>Start</button>
+      <button onClick={() => setIsRunning(false)}>Pause</button>
+      <button
+        onClick={() => {
+          setIsRunning(false);
+          setTimeLeft(customMinutes * 60); // convert m => s
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 }
