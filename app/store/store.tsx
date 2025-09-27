b24895847e92cef026f38d13fe0b8531a22b915b
Ngan <ngannguyen141123@gmail.com>
2025-09-27 21:02:26 +1000

    added persist for timer when reloading page

--- app/store/store.tsx
+++ app/store/store.tsx
@@ -1,12 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // local storage
 import timerReducer from "./timerSlice";
 
-export const store = configureStore({
-  reducer: {
-    timer: timerReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["timer"],
+};
+
+const rootReducer = combineReducers({
+  timer: timerReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 // type safe
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
