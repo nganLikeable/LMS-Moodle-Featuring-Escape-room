d670225b424bfab0bcb6908043d10417d9ee308b
Ngan <ngannguyen141123@gmail.com>
2025-10-08 22:45:18 +1100

    created new folder 'auth' , added auth file in lib, jwt_secret

---
+++ api/lib/auth.tsx
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+const JWT_SECRET = process.env.JWT_SECRET;
+
+if (!JWT_SECRET) {
+  throw new Error("Fatal error: JWT_SECRET not defined in .env file.");
+}
+
+// sign in a jwt token
+export const signToken = (user: { id: number; username: string }) => {
+  return jwt.sign({ sub: user.id, username: user.username }, JWT_SECRET, {
+    expiresIn: "1h",
+  });
+};
+
+// verify a jwt token
+export const verifyToken = (token: string) => {
+  try {
+    return jwt.verify(token, JWT_SECRET);
+  } catch (e) {
+    return null;
+  }
+};
