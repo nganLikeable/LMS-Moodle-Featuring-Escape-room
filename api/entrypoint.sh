32f3b6d1eed82613b89dc371cf982afe6786d461
Ngan <ngannguyen141123@gmail.com>
2025-10-16 22:21:57 +1100

    api route works, but not frontend app

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Wait for Postgres to be available
-./wait-for-it.sh postgres:5432 --timeout=30 --strict -- echo "✅ Postgres is up"
+./wait-for-it.sh postgres:5432 --timeout=30 --strict -- echo "Postgres is up"
 
 # Run Prisma commands
 npx prisma generate
