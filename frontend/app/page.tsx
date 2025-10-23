dd31b7f0bb7951bdb2bf0fa18fa89d7964e3b8b1
Ngan <ngannguyen141123@gmail.com>
2025-10-23 20:04:48 +1100

    modified home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,6 @@
 // app/page.tsx
-'use client'
-  
-const APIURL = "http://ec2-3-86-173-183.compute-1.amazonaws.com:4080";
-
+"use client";
+import Homepage from "./Components/Homepage";
 export default function Home() {
-  return ;
+  return <Homepage />;
 }
